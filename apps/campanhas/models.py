07cf69a687c72056ea211629c9cb3e38a3260667
Daniel Deitos Chiaramonte <12924111742@ulife.com.br>
2026-05-25 20:05:51 -0300

    feat: cria model CampanhaVacinacao

--- apps/campanhas/models.py
+++ apps/campanhas/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class CampanhaVacinacao(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    data_inicio = models.DateField()
+    data_fim = models.DateField()
+    publico_alvo = models.CharField(max_length=200)
+    ativa = models.BooleanField(default=True)
+
+    vacinas = models.ManyToManyField(
+        'vacinas.Vacina',
+        related_name='campanhas',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.nome
