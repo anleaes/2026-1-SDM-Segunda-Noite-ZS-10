77d9ba455e6dfbbddbc9493ce6c3a621e703f5dd
Guilherme Perlasca <perlasca47@gmail.com>
2026-05-25 00:27:38 -0300

    feat: cria model UnidadeSaude

--- unidades/models.py
+++ unidades/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class UnidadeSaude(models.Model):
+    nome = models.CharField(max_length=200)
+    endereco = models.CharField(max_length=300)
+    bairro = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+    horario_funcionamento = models.CharField(max_length=100)
+    ativa = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
