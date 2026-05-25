4c93bcbfa04886097808d6c48a7c0b104b43edd1
Guilherme Perlasca da Fontoura <1292424822@ulife.com.br>
2026-05-25 19:31:28 -0300

    feat: registra todas as urls dos apps no core

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     path('api/unidades/', include('apps.unidades.urls')),
     path('api/vacinas/', include('apps.vacinas.urls')),
     path('api/perfis/', include('apps.perfis.urls')),
-    path('api/calendario/', include('calendario.urls')),
-    path('api/atendimentos/', include('atendimentos.urls')),
-    path('api/registros/', include('registros.urls')),
+    path('api/calendario/', include('apps.calendario.urls')),
+    path('api/atendimentos/', include('apps.atendimentos.urls')),
+    path('api/registros/', include('apps.registros.urls')),
+    path('api/campanhas/', include('apps.campanhas.urls')),
+    path('api/notificacoes/', include('apps.notificacoes.urls')),
+    path('api/situacao/', include('apps.situacao.urls')),
 ]
