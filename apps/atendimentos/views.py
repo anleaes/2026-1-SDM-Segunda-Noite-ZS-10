958fb4a8ff7f3680c8673007aa88f56c1a3440a1
Fillipe Brito Pinto <britopintofillipe@gmail.com>
2026-05-25 18:52:25 -0300

    feat: cria viewsets para gerenciamento de atendimentos

--- apps/atendimentos/views.py
+++ apps/atendimentos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Atendimento, DoseAtendimento
+from .serializers import AtendimentoSerializer, DoseAtendimentoSerializer
 # Create your views here.
+class AtendimentoViewSet(viewsets.ModelViewSet):
+    queryset = Atendimento.objects.all()
+    serializer_class = AtendimentoSerializer
+
+class DoseAtendimentoViewSet(viewsets.ModelViewSet):
+    queryset = DoseAtendimento.objects.all()
+    serializer_class = DoseAtendimentoSerializer
