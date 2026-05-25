5431f40db320ee5c88d2e566cf25a26eb899245f
Daniel Deitos Chiaramonte <12924111742@ulife.com.br>
2026-05-25 20:19:51 -0300

    feat: cria model Notificacao

--- apps/notificacoes/models.py
+++ apps/notificacoes/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+TIPO_CHOICES = [
+    ('lembrete', 'Lembrete'),
+    ('alerta', 'Alerta'),
+    ('informativo', 'Informativo'),
+]
+
+class Notificacao(models.Model):
+
+    paciente = models.ForeignKey(
+        'pessoas.Paciente',
+        on_delete=models.CASCADE,
+        related_name='notificacoes'
+    )
+
+    titulo = models.CharField(max_length=200)
+    mensagem = models.TextField()
+
+    data_envio = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    tipo = models.CharField(
+        max_length=20,
+        choices=TIPO_CHOICES
+    )
+
+    lida = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.titulo} - {self.paciente}'
