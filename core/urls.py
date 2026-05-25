772fd87d2c7dab5f0000d3a6b97e52841807cea8
Daniel Deitos Chiaramonte <12924111742@ulife.com.br>
2026-05-25 20:39:16 -0300

    feat: registra todas as urls dos apps no core

--- core/urls.py
+++ core/urls.py
@@ -3,11 +3,15 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('api/pessoas/', include('apps.pessoas.urls')),
     path('api/unidades/', include('apps.unidades.urls')),
     path('api/vacinas/', include('apps.vacinas.urls')),
     path('api/perfis/', include('apps.perfis.urls')),
-    path('api/calendario/', include('calendario.urls')),
-    path('api/atendimentos/', include('atendimentos.urls')),
-    path('api/registros/', include('registros.urls')),
+    path('api/calendario/', include('apps.calendario.urls')),
+    path('api/atendimentos/', include('apps.atendimentos.urls')),
+    path('api/registros/', include('apps.registros.urls')),
+    path('api/campanhas/', include('apps.campanhas.urls')),
+    path('api/notificacoes/', include('apps.notificacoes.urls')),
+    path('api/situacao/', include('apps.situacao.urls')),
 ]
