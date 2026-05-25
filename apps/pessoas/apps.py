1355ba131d617501d9d19a4b31a8e8ed2fedb5d7
Guilherme Perlasca <perlasca47@gmail.com>
2026-05-25 15:41:02 -0300

    feat: reorganiza apps dentro da pasta apps/

--- apps/pessoas/apps.py
+++ apps/pessoas/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RegistrosConfig(AppConfig):
+class PessoasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'registros'
+    name = 'apps.pessoas'
--- apps/vacinas/apps.py
+++ apps/vacinas/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AtendimentosConfig(AppConfig):
+class VacinasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'atendimentos'
+    name = 'apps.vacinas'
