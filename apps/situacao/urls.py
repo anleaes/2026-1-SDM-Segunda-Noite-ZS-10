dcfe575350b4f6491ba208e72e102e39300ccac6
Daniel Deitos Chiaramonte <12924111742@ulife.com.br>
2026-05-25 20:36:04 -0300

    feat: configura router e urls de situacao

---
+++ apps/situacao/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+from .views import SituacaoVacinaViewSet
+
+router = DefaultRouter()
+
+router.register(
+    r'situacao',
+    SituacaoVacinaViewSet
+)
+
+urlpatterns = router.urls
