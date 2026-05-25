46980dc0bd9ca3437988a408bb69212be878fc07
Daniel Deitos Chiaramonte <12924111742@ulife.com.br>
2026-05-25 20:24:42 -0300

    feat: configura router e urls de notificacoes

---
+++ apps/notificacoes/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+from .views import NotificacaoViewSet
+
+router = DefaultRouter()
+
+router.register(
+    r'notificacoes',
+    NotificacaoViewSet
+)
+
+urlpatterns = router.urls
